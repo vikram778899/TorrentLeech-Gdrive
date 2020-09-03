7d8e8ab3b62ce779f8590a9109a9e66e54c6e9f8
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2020-09-03 11:03:25 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -125,7 +125,6 @@ class CloneHelper:
                 
             LOGGER.info(gautii)
             gau_link = re.search("(?P<url>https?://[^\s]+)", gautii).group("url")
-	    td_join = f"https://groups.google.com/u/0/g/test_group005"
             LOGGER.info(gau_link)
             button = []
             button.append(
@@ -144,7 +143,7 @@ class CloneHelper:
                 tam_link = requests.utils.requote_uri(indexurl)
                 LOGGER.info(tam_link)
                 button.append([pyrogram.InlineKeyboardButton(text="ℹ️ IndexUrl ℹ️", url=f"{tam_link}")])
-		button.append([pyrogram.InlineKeyboardButton(text="📂 Join TeamDrive 📂", url=f"{td_join}")])
+		button.append([pyrogram.InlineKeyboardButton(text="📂 Join TeamDrive 📂", url=f"https://groups.google.com/u/0/g/test_group005")])
             button_markup = pyrogram.InlineKeyboardMarkup(button)
             msg = await self.lsg.edit_text(
                 f"🤖: {_up} cloned successfully in your Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
