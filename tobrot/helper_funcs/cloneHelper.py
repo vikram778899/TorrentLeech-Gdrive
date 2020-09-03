9810c27515777e83c37e68b3823f54618b56e4cd
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2020-09-03 10:10:54 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -140,9 +140,10 @@ class CloneHelper:
                     indexurl = f"{INDEX_LINK}/{self.name}"
                 else:
                     indexurl = f"{INDEX_LINK}/{self.name}/"
-                tam_link = requests.utils.requote_uri(indexurl)
-		td_link = f"https://groups.google.com/u/0/g/test_group005"
+                tam_link = requests.utils.requote_uri(indexurl) td_link = f"https://groups.google.com/u/0/g/test_group005
+		
                 LOGGER.info(tam_link)
+		
 		button.append([pyrogram.InlineKeyboardButton(text="📂 Join TeamDrive 📂", url=f"{td_link}")])
                 button.append([pyrogram.InlineKeyboardButton(text="ℹ️ IndexUrl ℹ️", url=f"{tam_link}")])
             button_markup = pyrogram.InlineKeyboardMarkup(button)
