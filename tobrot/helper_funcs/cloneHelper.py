823c68c9d799b4272cbdf0eea83e3aa20056eaab
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2020-09-03 11:23:44 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -135,8 +135,7 @@ class CloneHelper:
                     )
             ]
             )
-            if INDEX_LINK:
-		td_join = f"https://groups.google.com/u/0/g/test_group005"	
+            if INDEX_LINK:	
                 if _flag == "--files-only":
                     indexurl = f"{INDEX_LINK}/{self.name}"
                 else:
@@ -144,7 +143,6 @@ class CloneHelper:
                 tam_link = requests.utils.requote_uri(indexurl)
                 LOGGER.info(tam_link)
                 button.append([pyrogram.InlineKeyboardButton(text="ℹ️ IndexUrl ℹ️", url=f"{tam_link}")])
-	        button.append([pyrogram.InlineKeyboardButton(text="📂 Join TeamDrive 📂", url=f"{td_join}")])
             button_markup = pyrogram.InlineKeyboardMarkup(button)
             msg = await self.lsg.edit_text(
                 f"🤖: {_up} cloned successfully in your Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
